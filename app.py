1e049edfb729c8162b001029885ab38450079dd2
StevenLomon <steven.lennartsson@gmail.com>
2024-03-27 08:08:21 +0100

    Playing around with Redis to store rate limits

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime, redis
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
@@ -10,3 +10,31 @@ my_dag = DAG(
 )
  
 EmptyOperator(task_id="task")
+
+r = redis.Redis(
+  host='redis-18175.c327.europe-west1-2.gce.cloud.redislabs.com',
+  port=18175,
+  password='tfIixO8DhVjcWCDVjg3gBjuy3gYDyDZ7')
+
+res1 = r.hset(
+    "bike:1",
+    mapping={
+        "model": "Deimos",
+        "brand": "Ergonom",
+        "type": "Enduro bikes",
+        "price": 4972,
+    },
+)
+
+print(res1)
+
+res2 = r.hget("bike:1", "model")
+print(res2)
+
+res3 = r.hget("bike:1", "price")
+print(res3)
+
+res4 = r.hgetall("bike:1")
+print(res4)
+
+print(r)
